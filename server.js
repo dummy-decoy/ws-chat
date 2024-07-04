be20ac595da1e8c46bb719295396108f4dbc977f
dummy-decoy <79310325+dummy-decoy@users.noreply.github.com>
2024-07-05 00:32:00 +0200

    modes and moderation
    - channel modes
    - user modes
    - moderation commands

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const http = require('http');
 const ws = require('ws');
 const fs = require('fs');
 
+class UserOptions {admin = false; bot = false};
+class ChannelOptions {password = undefined; moderated = false; secret = false; topic = false};
+class ChannelUserOptions {operator = false; voiced = true};
 
 class User {
     url = '';
     nick = undefined;
-    mode = 0;
+    options = new UserOptions();
     channels = new Map();
     handler = new Map();
 
@@ -21,8 +24,8 @@ class User {
 class Channel {
     name = '';
     topic = '';
-    mode = 0;
-    users = new Set();
+    options = new ChannelOptions();
+    users = new Map();
 
     constructor(name) {
         this.name = name;
@@ -34,13 +37,31 @@ class Chat {
     users = new Set();
     channels = new Map();
     
-    _leaveChannel(user, channel, reason) {
-        channel.users.forEach(function (dest) {dest.handler.get('leave')(user.nick, channel.name, reason);});
-        channel.users.delete(user);
-        if (channel.users.size == 0) {
-            this.channels.delete(channel.name);
+    _joinChannel(user, channel) {
+        let joined = this.channels.get(channel);
+        if (!joined) {
+            joined = new Channel(channel);
+            joined.users.set(user, {...new ChannelUserOptions(), operator:true});
+            this.channels.set(channel, joined);
+        } else { 
+            joined.users.set(user, {...new ChannelUserOptions(), 'voiced': !joined.options.moderated});
         }
+        for (let [dest, options] of joined.users) { dest.handler.get('join')(user.nick, joined.name); };
+        return joined;
+    }
+    _cleanupChannel(user, channel) {
+        channel.users.delete(user);
+        if (channel.users.size == 0)
+            this.channels.delete(channel.name);
+    }
+    _leaveChannel(user, channel, reason) {
+        for (let [dest, options] of channel.users) { dest.handler.get('leave')(user.nick, channel.name, reason); };
+        this._cleanupChannel(user, channel);
     };
+    _kickUser(user, channel, reason) {
+        for (let [dest, options] of channel.users) { dest.handler.get('kick')(user.nick, channel.name, reason); };
+        this._cleanupChannel(user, channel);
+    }
     _validateNick(nick) {
         return (nick.length <= 25) && (/^[^@#&!\s]+$/.test(nick));
     };
@@ -65,17 +86,23 @@ class Chat {
     };
     disconnect(user, reason) {
         console.log(`${user.nick}@${user.url} is quitting (${reason})`);
-        for (let channel of user.channels.values()) {
+        for (let channel of user.channels.values())
             this._leaveChannel(user, channel, `${user.nick} has left the building !`);
-        }
         this.users.delete(user);
     };
 
     timeq(user) {
         user.handler.get('time')(Date().toString());
     };
-    motdq(user) {
-        user.handler.get('motd')(this.motd);
+    motdq(user, value=undefined) {
+        if (typeof value === 'undefined') {
+            user.handler.get('motd')(this.motd);
+        } else {
+            if (!user.options.admin)
+                return user.handler.get('error')('you are not allowed to modify server motd');
+            this.motd = value;
+            for (let dest of this.users) { dest.handler.get('motd')(this.motd) };
+        }
     };
     nickq(user, newnick) {
         if (!this._validateNick(newnick))
@@ -83,56 +110,67 @@ class Chat {
         let oldnick = user.nick;
         user.nick = newnick;
         user.handler.get('nick')(newnick);
-        user.channels.forEach(function (channel) {channel.users.forEach(function (dest) {dest.handler.get('nick')(newnick, oldnick, channel.name);})});
+        for (let channel of user.channels) { channel.users.forEach(function (options,dest) {dest.handler.get('nick')(newnick, oldnick, channel.name);}) };
     };
     listq(user) {
-        user.handler.get('list')(Array.from(this.channels.keys()));
+        user.handler.get('list')(Array.from(Array.from(this.channels.values()).filter((channel) => user.options.admin || user.channels.has(channel.name) || !channel.options.secret).map((channel) => [channel.name, {...channel.options, password: !!channel.options.password}])));
     };
-    usersq(user, channel) {
-        if (!user.channels.has(channel))
-            return user.handler.get('error')(`not participating in channel ${channel}`);
-        user.handler.get('users')(channel, Array.from(this.channels.get(channel).users).map(user=>user.nick));
+    usersq(user, channel = undefined) {
+        console.log(channel);
+        if (typeof channel == 'undefined') {
+            if (!user.options.admin)
+                return user.handler.get('error')('insufficient privileges');
+            user.handler.get('users')(undefined, Array.from(this.users).map(user => [user.nick,user.options,user.url]));
+        } else {
+            if (!user.channels.has(channel))
+                return user.handler.get('error')(`not participating in channel ${channel}`);
+            user.handler.get('users')(channel, Array.from(this.channels.get(channel).users).map(([user,options]) => [user.nick, {...user.options,...options},user.url]));
+        }
     };
-    joinq(user, channel) {
+    joinq(user, channel, password=undefined) {
         if (!this._validateChannel(channel))
             return user.handler.get('error')('invalid name for a channel (max 25 letters, does not contain whitespace nor @ # & !)');
         if (!user.nick)
             return user.handler.get('error')('please choose your nickname first');
         if (user.channels.has(channel))
             return user.handler.get('error')(`channel ${channel} already joined`);
-        if (!this.channels.has(channel))
-            this.channels.set(channel, new Channel(channel));
-        let joined = this.channels.get(channel);
-        joined.users.add(user);
+        if (this.channels.has(channel) && this.channels.get(channel).options.password && (this.channels.get(channel).options.password != password) && !user.options.admin)
+            return user.handler.get('error')(`a password is required to join channel ${channel}`);
+        let joined = this._joinChannel(user, channel);
         user.channels.set(channel, joined);
-        joined.users.forEach(function (dest) {dest.handler.get('join')(user.nick, joined.name);});
         this.usersq(user, channel);
         this.topicq(user, channel);
     };
     topicq(user, channel, value=undefined) {
         if (!user.channels.has(channel))
             return user.handler.get('error')(`not participating in channel ${channel}`);
-        if (typeof value === 'undefined') {
-            user.handler.get('topic')(channel, this.channels.get(channel).topic);
+        if (typeof value == 'undefined') {
+            return user.handler.get('topic')(channel, this.channels.get(channel).topic);
         } else {
+            let target = this.channels.get(channel);
+            if (target.options.topic && !channel.users.get(user).options.operator)
+                return user.handler.get('error')('you are not allowed to modify channel topic');
             if (!this._validateTopic(value))
                 return user.handler.get('error')('invalid topic (max 200 letters)');
-            this.channels.get(channel).topic = value;
-            this.channels.get(channel).users.forEach(function (dest) {dest.handler.get('topic')(channel, value)});
+            target.topic = value;
+            for (let dest of target.users.keys()) { dest.handler.get('topic')(channel, value); };
         }
-    }
+    };
     msgq(user, channel, message) {
         if (!user.channels.has(channel))
             return user.handler.get('error')(`not participating in channel ${channel}`);
+        let target = this.channels.get(channel);
+        if (target.options.moderated && !target.users.get(user).operator && !target.users.get(user).voiced && !user.options.admin)
+            return user.handler.get('error')('you are not allowed to talk');
         if (!this._validateMessage(message))
             return user.handler.get('error')('invalid message (max 1000 characters)');
-        this.channels.get(channel).users.forEach(function (dest) {dest.handler.get('msg')(user.nick, channel, message);});
+        for (let dest of target.users.keys()) { dest.handler.get('msg')(user.nick, channel, message); };
     };
     privmsgq(user, target, message) {
         if (!user.nick)
             return user.handler.get('error')('please choose your nickname first');
         let findUser = (nick) => {
-            for (let user of this.users) if (user.nick == nick) return user;
+            for (let user of this.users.keys()) if (user.nick == nick) return user;
             return undefined;
         }
         let dest = findUser(target);
@@ -153,6 +191,109 @@ class Chat {
         let leaved = user.channels.get(channel);
         this._leaveChannel(user, leaved, reason);
         user.channels.delete(channel);
+    };
+    channelmodeq(user, channel, options) {
+        if (!this.channels.has(channel))
+            return user.handler.get('error')(`channel ${channel} does not exist`);
+        let target = this.channels.get(channel);
+        if (typeof options == 'undefined') {
+            if (target.options.secret && !user.channels.has(channel) && !user.options.admin)
+                return user.handler.get('error')(`channel ${channel} does not exist`);
+            user.handler.get('channelmode')(channel, {...target.options, password: !!target.options.password});
+        } else {
+            if (!target.users.has(user) && !target.users.get(user).operator && !user.options.admin)
+                return user.handler.get('error')('you are not allowed to change channel modes');
+            // do not trust user input to store those permissions
+            if ('topic' in options) target.options.topic = !!options.topic;
+            if ('moderated' in options) target.options.moderated = !!options.moderated;
+            if ('secret' in options) target.options.secret = !!options.secret;
+            if ('password' in options) { if (!options.password) target.options.password = undefined; else target.options.password = options.password; };
+            for (let dest of target.users.keys()) { dest.handler.get('channelmode')(channel, {...target.options, password: !!target.options.password}); };
+            if (!user.channels.has(channel)) user.handler.get('channelmode')(channel, {...target.options, password: !!target.options.password});
+        }
+    };
+    chanusermodeq(user, channel, victim, options) {
+        if (!this.channels.has(channel))
+            return user.handler.get('error')(`channel ${channel} does not exist`);
+        let target = this.channels.get(channel);
+        let findUser = (nick) => {
+            for (let user of this.users.keys()) if (user.nick == nick) return user;
+            return undefined;
+        }
+        let dest = findUser(victim);
+        if (typeof dest === 'undefined')
+            return user.handler.get('error')(`user ${victim} not found`);
+        if (!dest.channels.has(channel))
+            return user.handler.get('error')(`user ${victim} does not participate in channel ${channel}`);
+        if (typeof options == 'undefined') {
+            if (target.options.secret && !user.channels.has(channel) && !user.options.admin)
+                return user.handler.get('error')(`channel ${channel} does not exist`);
+            user.handler.get('chanusermode')(channel, victim, {...target.users.get(dest), ...dest.options});
+        } else {
+            if (!target.users.has(user) && !target.users.get(user).operator && !user.options.admin)
+                return user.handler.get('error')('you are not allowed to change user modes on this channel');
+            // do not trust user input to store those permissions
+            if ('operator' in options) target.users.get(dest).operator = !!options.operator;
+            if ('voiced' in options) target.users.get(dest).voiced = !!options.voiced;
+            for (let peon of target.users.keys()) { peon.handler.get('chanusermode')(channel, victim, target.users.get(dest)); };
+            if (!user.channels.has(channel)) user.handler.get('chanusermode')(channel, victim, target.users.get(dest));
+        }
+    };
+    usermodeq(user, victim, options) {
+        let findUser = (nick) => {
+            for (let user of this.users.keys()) if (user.nick == nick) return user;
+            return undefined;
+        }
+        let dest = findUser(victim);
+        if (typeof dest === 'undefined')
+            return user.handler.get('error')(`user ${victim} not found`);
+        if (typeof options == 'undefined') {
+            user.handler.get('usermode')(dest.nick, dest.options);
+        } else {
+            if (('admin' in options) && !user.options.admin)
+                return user.handler.get('error')('insufficient privileges');
+            if (('bot' in options) && !user.options.admin && (user != dest))
+                return user.handler.get('error')('you cannot make other people feel like a bot !');
+            // do not trust user input to store those permissions
+            if ('admin' in options) dest.options.admin = !!options.admin;
+            if ('bot' in options) dest.options.bot = !!options.bot;
+            dest.handler.get('usermode')(dest.nick, dest.options);
+            if (user != dest) user.handler.get('usermode')(dest.nick, dest.options);
+        }
+    };
+    kickq(user, channel, victim, reason) {
+        if (!user.channels.has(channel))
+            return user.handler.get('error')(`not participating in channel ${channel}`);
+        if (!user.channels.get(channel).users.get(user).operator && !user.options.admin)
+            return user.handler.get('error')(`you have no right to kick ${victim} out of ${channel}`);
+        let findUser = (nick) => {
+            for (let user of this.users.keys()) if (user.nick == nick) return user;
+            return undefined;
+        }
+        let dest = findUser(victim);
+        if (typeof dest === 'undefined')
+            return user.handler.get('error')(`user ${victim} not found`);
+        if (!dest.channels.has(channel))
+            return user.handler.get('error')(`user ${victim} is not guilty of anything in channel ${channel}`);
+        if (!this._validateReason(reason))
+            return user.handler.get('error')('invalid reason (max 50 characters)');
+        let kicked = user.channels.get(channel);
+        this._kickUser(dest, kicked, reason);
+        dest.channels.delete(channel);
+    };
+    killq(user, victim, reason) {
+        if (!user.options.admin)
+            return user.handler.get('error')('there is no such thing as free kill !');
+        let findUser = (nick) => {
+            for (let user of this.users.keys()) if (user.nick == nick) return user;
+            return undefined;
+        }
+        let dest = findUser(victim);
+        if (typeof dest === 'undefined')
+            return user.handler.get('error')(`user ${victim} not found`);
+        if (!this._validateReason(reason))
+            return user.handler.get('error')('invalid reason (max 50 characters)');
+        this._kickUser(dest, reason);
     };
 };
 var chat = new Chat();
@@ -177,6 +318,11 @@ class Protocol {
         user.on('msg', this.msg.bind(ws));
         user.on('privmsg', this.privmsg.bind(ws));
         user.on('leave', this.leave.bind(ws));
+        user.on('channelmode', this.channelmode.bind(ws));
+        user.on('chanusermode', this.chanusermode.bind(ws));
+        user.on('usermode', this.usermode.bind(ws));
+        user.on('kick', this.kick.bind(ws));
+        user.on('kill', this.kill.bind(ws));
 
         this.version.bind(ws)();
         chat.motdq(user);
@@ -201,7 +347,7 @@ class Protocol {
                         this.error.bind(ws)('unrecognized protocol version');
                     break;
                 case 'motd':
-                    chat.motdq(user);
+                    chat.motdq(user, (message[1] && ('value' in message[1])) ? message[1].value : undefined);
                     break;
                 case 'time':
                     chat.timeq(user);
@@ -213,10 +359,20 @@ class Protocol {
                     chat.listq(user);
                     break;
                 case 'users':
-                    chat.usersq(user, message[1].channel);
+                    chat.usersq(user, (message[1] && ('channel' in message[1])) ? message[1].channel : undefined);
                     break;
                 case 'join':
-                    chat.joinq(user, message[1].channel);
+                    chat.joinq(user, message[1].channel, 'password' in message[1] ? message[1].password : undefined);
+                    break;
+                case 'mode':
+                    if ('channel' in message[1]) {
+                        if ('user' in message[1])
+                            chat.chanusermodeq(user, message[1].channel, message[1].user, 'options' in message[1] ? message[1].options : undefined);
+                        else
+                            chat.channelmodeq(user, message[1].channel, 'options' in message[1] ? message[1].options : undefined);
+                    } else {
+                        chat.usermodeq(user, message[1].user, 'options' in message[1] ? message[1].options : undefined)
+                    };
                     break;
                 case 'topic':
                     chat.topicq(user, message[1].channel, 'value' in message[1] ? message[1].value : undefined);
@@ -229,6 +385,12 @@ class Protocol {
                     break;
                 case 'leave':
                     chat.leaveq(user, message[1].channel, 'reason' in message[1] ? message[1].reason : undefined);
+                    break;
+                case 'kick':
+                    chat.kickq(user, message[1].channel, message[1].user, 'reason' in message[1] ? message[1].reason : undefined);
+                    break;
+                case 'kill':
+                    chat.killq(user, message[1].user, 'reason' in message[1] ? message[1].reason : undefined);
                     break;
             }
         } catch (error) {
@@ -272,6 +434,21 @@ class Protocol {
     };
     leave(user, channel, reason) {
         this.send(JSON.stringify(['leave', {'user': user, 'channel': channel, 'reason': reason}]));
+    };
+    channelmode(channel, options) {
+        this.send(JSON.stringify(['mode', {'channel': channel, 'options': options}]))
+    };
+    chanusermode(channel, user, options) {
+        this.send(JSON.stringify(['mode', {'channel': channel, 'user': user, 'options': options}]));
+    };
+    usermode(user, options) {
+        this.send(JSON.stringify(['mode', {'user': user, 'options': options}]));
+    };
+    kick(user, channel, reason) {
+        this.send(JSON.stringify(['kick', {'user': user, 'channel': channel, 'reason': reason}]));
+    };
+    kill(user, reason) {
+        this.send(JSON.stringify(['kill', {'user': user, 'reason': reason}]));
     };
 };
 var protocol = new Protocol();
